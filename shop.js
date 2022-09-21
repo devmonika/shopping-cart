709b74572419ec0503af65fcea512366fb0061a4
rssabbir <sabbirhossainrahad@gmail.com>
2022-09-22 01:30:46 +0600

    restore old commit

--- shop.js
+++ shop.js
@@ -42,7 +42,7 @@ const loadAllCat = async (category) => {
 // get all product
 
 const getShopApi = async () => {
-	const url = `https://fakestoreapi.com/products`;
+	const url =`https://fakestoreapi.com/products`;
 	const res = await fetch(url);
 	const data = await res.json();
 	showShopData(data);
@@ -52,8 +52,8 @@ const getShopApi = async () => {
 //global variable array
 let cart = JSON.parse(localStorage.getItem('cart'));
 
-const addItem = async (productID) => {
-	const product = await productForCart(productID).then((data) => data);
+const addItem = async(productID) =>{
+  const product = await productForCart(productID).then(data=> data);
 	if (!cart.length) {
 		cart.push(product);
 	} else {
@@ -65,51 +65,57 @@ const addItem = async (productID) => {
 	localStorage.setItem('cart', JSON.stringify(cart));
 	displayCartLength(cart);
 	showCartItems(product);
-};
+}
 
 const displayCartLength = (cart) => {
 	const display = document.getElementById('add-item');
 	display.innerText = cart.length;
 };
-const cartIcon = document.getElementById('cart-icon');
-cartIcon.addEventListener('click', () => {
-	cartIcon.toggleAttribute('style', 'display:block');
-});
-// show cart items
-const showCartItems = (product) => {
+// show cart items 
+const showCartItems = (product) =>{
 	const display = document.getElementById('show-cart');
-	const cartContainer = document.getElementById('cart-container');
 	const container = document.createElement('div');
-	if (display) {
-		cartContainer.style.display = 'block';
-	}
-
-	container.innerHTML = `
-	<div class="p-2 w-12"><img src="${
-		product.image ? product.image : 'no img found'
-	}" alt="img product"></div>
-	<div class="flex-auto text-sm w-32">
-	<div class="font-bold">${product.title ? product.title : 'no title found'}</div>
-	<div class="truncate">${product.description.slice(0, 20)}</div>
-	<div class="text-gray-400">Qt: ${product.cart}</div>
+	container.innerHTML=`
+	<div class="p-2 flex bg-white hover:bg-gray-100 cursor-pointer border-b border-gray-100" style="">
+		<div class="p-2 w-12"><img src="${
+			product.image ? product.image : 'no img found'
+		}" alt="img product"></div>
+		<div class="flex-auto text-sm w-32">
+			<div class="font-bold">${
+				product.title ? product.title : 'no title found'
+			}</div>
+			<div class="truncate">${product.description.slice(0, 20)}</div>
+			<div class="text-gray-400">Qt: ${product.cart}</div>
+		</div>
+		<div class="flex flex-col w-18 font-medium items-end">
+			<div class="w-4 h-4 mb-6 hover:bg-red-200 rounded-full cursor-pointer text-red-700">
+				<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2 ">
+					<polyline points="3 6 5 6 21 6"></polyline>
+					<path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+					<line x1="10" y1="11" x2="10" y2="17"></line>
+					<line x1="14" y1="11" x2="14" y2="17"></line>
+				</svg>
+			</div>
+			$${product.price}</div>
 	</div>
-	<div class="flex flex-col w-18 font-medium items-end">
-	<div class="w-4 h-4 mb-6 hover:bg-red-200 rounded-full cursor-pointer text-red-700">
-		<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24"
-		stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-		class="feather feather-trash-2 ">
-		<polyline points="3 6 5 6 21 6"></polyline>
-		<path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-		<line x1="10" y1="11" x2="10" y2="17"></line>
-		<line x1="14" y1="11" x2="14" y2="17"></line>
-		</svg>
-	</div>
-	$${product.price}
-	</div>
+    <div class="p-4 justify-around flex ">
+        <button class="text-base  undefined  hover:scale-110 focus:outline-none flex justify-center px-4 py-2 rounded font-bold cursor-pointer 
+      hover:bg-teal-700 hover:text-teal-100 
+      bg-teal-100 
+      text-teal-700 
+      border duration-200 ease-in-out 
+      border-teal-600 transition">Checkout</button>
+	  <button class="text-base  undefined  hover:scale-110 focus:outline-none flex justify-center px-4 py-2 rounded font-bold cursor-pointer 
+      hover:bg-teal-700 hover:text-teal-100 
+      bg-teal-100 
+      text-teal-700 
+      border duration-200 ease-in-out 
+      border-teal-600 transition">View Cart</button>
+    </div>
 	`;
 	display.appendChild(container);
 	// console.log(product);
-};
+}
 
 // show all product
 
@@ -187,6 +193,8 @@ const showProductDetails = (shopData) => {
   `;
 	showDetailsOfProduct.appendChild(modalBox);
 };
+
+
 
 getCatUrl();
 // productPOpUp();
